b057dc5abc400b035a32e9481da60d734aa8f2cf
Marcel <Marcel@Marcel-PC>
2015-01-14 01:27:25 +0100

    Abfrage DB und neue DB anlgen für dienst.php

--- dienst.php
+++ dienst.php
@@ -5,7 +5,7 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <?php
-static $rfid = '100890';
+static $rfid = '271088';
 $tableid = $_GET["idtable"];
 
 ?>
